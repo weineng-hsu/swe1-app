28e43b5f0e4849fa716280a9cc021e6b94830f90
weineng-hsu <weineng.hsu@gmail.com>
2022-03-05 01:48:25 -0500

    black reformatting

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -21,6 +21,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-
-
